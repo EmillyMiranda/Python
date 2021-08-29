37c436dc9f0d934c3ce6dddcbd5cd028ec41b12d
EmillyMiranda <87238788+EmillyMiranda@users.noreply.github.com>
2021-08-29 14:42:00 +0100

    exercício2

---
+++ ex003.py
@@ -0,0 +1,9 @@
+n = (input('Digite algo:'))
+print ('O tipo primitivo é', type(n))
+print ('Só tem espaços?',n.isspace())
+print ('É um número?', n.isalpha())
+print ('Está em maiúsculo?',n.isupper())
+print ('Está em minusculo?',n.islower())
+print ('Está descapitalizada?',n.istitle())
+print ('è alfanúmerico?',n.isalnum())
+
---
+++ ex004.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um valor:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print (' O valor {}, o dobro é {} o triplo é {} e a raiz quadrada é {}'.format(n, d, t, r))
